c0b4ef5538d2a6a8a023ac11408dee7fe70fd948
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2023-04-23 23:26:28 -0500

    ADD: explicacion de flex align-items

--- src/screens/FlexBoxScreen.tsx
+++ src/screens/FlexBoxScreen.tsx
@@ -6,21 +6,6 @@ export const FlexBoxScreen = () => {
       <Text style={styles.box1}>Caja1</Text>
       <Text style={styles.box2}>Caja2</Text>
       <Text style={styles.box3}>Caja3</Text>
-      <Text style={styles.box3}>Caja3</Text>
-      <Text style={styles.box3}>Caja3</Text>
-      <Text style={styles.box3}>Caja3</Text>
-      <Text style={styles.box3}>Caja3</Text>
-      <Text style={styles.box3}>Caja3</Text>
-      <Text style={styles.box3}>Caja3</Text>
-      <Text style={styles.box3}>Caja3</Text>
-      <Text style={styles.box3}>Caja3</Text>
-      <Text style={styles.box3}>Caja3</Text>
-      <Text style={styles.box3}>Caja3</Text>
-      <Text style={styles.box3}>Caja3</Text>
-      <Text style={styles.box3}>Caja3</Text>
-      <Text style={styles.box3}>Caja3</Text>
-      <Text style={styles.box3}>Caja3</Text>
-      <Text style={styles.box3}>Caja3</Text>
     </View>
   )
 }
@@ -28,27 +13,22 @@ export const FlexBoxScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexWrap: "wrap",
     backgroundColor: "#28C4D9",
     flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
   },
   box1: {
-    width: 100,
-    height: 100,
     borderWidth: 2,
     borderColor: "white",
     fontSize: 30
   },
   box2: {
-    width: 100,
-    height: 100,
     borderWidth: 2,
     borderColor: "white",
     fontSize: 30
   },
   box3: {
-    width: 100,
-    height: 100,
     borderWidth: 2,
     borderColor: "white",
     fontSize: 30
